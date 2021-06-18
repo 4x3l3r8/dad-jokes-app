0e14e5553a2c201a2ab58c92b8a880e33effac82
[Adesanya <[danieladesanya25@gmail.com]>
2021-06-18 19:20:00 +0100

    Initial commit 'personal'

---
+++ src/components/Joke.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+export default function Joke() {
+  const [joke, setJoke] = useState("");
+
+  return (
+    <div className="mt-4">
+      <div className="card">
+        <div className="card-header">Dad Joke</div>
+        <div className="card-body">
+          {/* <h5 className="card-title">Special title treatment</h5> */}
+          <p className="card-text">{joke.joke}</p>
+          <a href="#" className="btn btn-primary">
+            Get Another Joke
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
